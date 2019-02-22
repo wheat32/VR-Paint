28218102d68f524630c1209bf6bfedbb9c2c8b89
jackrogers453 <47458908+jackrogers453@users.noreply.github.com>
2019-02-22 14:05:41 -0500

    Added Switchable Skyboxes
    SkyBoxes can now be switched using 'A' or 'B'

---
+++ Assets/Scripts/SkyBox Scripts/SkyBoxSwitch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxSwitch : MonoBehaviour
+{
+
+    public Material mat1;
+    public Material mat2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RenderSettings.skybox = mat1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            RenderSettings.skybox = mat2;
+        }
+
+    }
+}
+
+
+
+
+
+
